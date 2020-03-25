c58d76bb80a7a30149f9add28298e7596964b47b
Tri <trideep.india@gmail.com>
2020-03-26 01:04:52 +0530

    Required Changes

--- upload.php
+++ upload.php
@@ -2,18 +2,19 @@
 session_start();
 $con=mysqli_connect("localhost","root","","ps");
 $gname=$_POST['gname'];
-$tmp_name=$_SESSION['fname'];
-$id="SELECT id FROM signup WHERE fname='$tmp_name';";
-$id_result=mysqli_query($con,$id) or die(mysqli_error($con));
-$duplicate="SELECT * FROM profile WHERE gname='$gname' AND id='$id_result';";
+$uname=$_SESSION['uname'];
+
+$duplicate="SELECT * FROM profile WHERE uname='$uname' AND gname='$gname';";
 $duplicate_result=mysqli_query($con,$duplicate) or die(mysqli_error($con));
-if(mysqli_num_rows($duplicate_result)==1){
+if(mysqli_num_rows($duplicate_result)!=0){
 	echo "Group name already taken!";
 	header('Location:insert.php');
 }
 else{
-	$sql="INSERT INTO profile(gname) VALUES('$gname')";
+	$sql="INSERT INTO profile(uname,gname) VALUES('$uname','$gname');";
 	$sql_result=mysqli_query($con,$sql);
 	header("Location:main.php");
 }
+
+
 ?>
